1628f148eccbed5227a90e7d83757624f33b1925
BingyuanXu <xubingyuan123@hotmail.com>
2020-05-29 14:01:23 -0500

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by bingyuan xu on 2020-05-29.
+//  Copyright © 2020 bingyuan xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
