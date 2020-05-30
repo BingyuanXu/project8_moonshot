21c7eee3637ca9f3d5ddc0d3c07ceb5ab3a6cc45
BingyuanXu <xubingyuan123@hotmail.com>
2020-05-30 11:56:48 -0500

    add scrollview and how to fit image

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,13 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  @State private var show = true
+  var body: some View {
+    NavigationView {
+      
+      VStack {
+        ScrollView(.vertical) {
+          VStack(spacing: 10) {
+            ForEach(0 ..< 100) {
+              Text("Item \($0)")
+                .font(.title)
+            }
+          }
+          .frame(maxWidth : .infinity)
+        }
+        Text(self.show ? "true" : "false")
+        GeometryReader {geo in
+          Image("example")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: geo.size.width)          //          .clipped()
+        }
+        
+      }
+        
+      .navigationBarItems(trailing:
+        Button(action: {
+          self.show.toggle()
+          print("something")
+        }) {
+          Image(systemName: "trash")
+      })
     }
+    .edgesIgnoringSafeArea(.leading)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
